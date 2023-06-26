778e82ee91b694202cd03d3bb3c5de87180213a6
mlichlundia <mlichlundiapl@gmail.com>
2023-06-26 17:00:25 +0200

    fix: fix move command

--- src/utils/fs/move.js
+++ src/utils/fs/move.js
@@ -6,6 +6,8 @@ import { rm } from 'node:fs/promises';
 import { getFileName } from '../base/getFileName.js';
 
 const move = async (currentDir, file, target) => {
+    let hasError = false
+
     if(!currentDir || !file || !target) {
         console.error(INVALID_INPUT, 1)
         return
@@ -14,17 +16,21 @@ const move = async (currentDir, file, target) => {
     const path = resolve(currentDir, file)
     const targetPath = resolve(currentDir, target)
 
-    if(!await isFile(path)) {
+    if(!await (isFile(path) || isFile(targetPath))) {
         return
     }
 
     const readStream = createReadStream(path)
     const writeStream = createWriteStream(targetPath + '/' + getFileName(file))
 
-    readStream.pipe(writeStream).on('error', () => {console.error(OPERATION_FAILD);readStream.close()})
+    readStream.pipe(writeStream).on('error', () => {
+        console.error(OPERATION_FAILD)
+        hasError = true
+        readStream.close()
+    })
     
     await new Promise((resolve) => readStream.on('close', async() => {
-        await rm(path)
+        !hasError && await rm(path)
         resolve()
     }))
 };
